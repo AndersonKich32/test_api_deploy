595935717b9e338dd9b999a6bec2b7ffed012a7e
Your Name <you@example.com>
2021-01-19 10:06:38 -0300

    ultimo

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,8 +5,8 @@ console.log(usuario, senha)
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host : 'localhost',  
-        user : 'root',      
+        host : 'mysql742.umbler.com',  
+        user : 'testapi',      
         password : '97408469Bdc',  
         database : 'apitest',
         multiStatements: true      
