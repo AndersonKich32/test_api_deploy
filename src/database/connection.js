6cbe562f1e6f794a3a3340f91cc33536b3010851
Your Name <you@example.com>
2021-01-19 09:47:51 -0300

    novo

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,10 +5,11 @@ console.log(usuario, senha)
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host : 'mysql669.umbler.com',  
-        user : 'test_api',      
-        password : '97408469',  
-        database : 'todo'       
+        host : 'localhost',  
+        user : 'root',      
+        password : '97408469Bdc',  
+        database : 'apitest',
+        multiStatements: true      
      }
 });
 module.exports = knex
